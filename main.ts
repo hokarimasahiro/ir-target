1cd930acb34795357db7d2619ce0da3f458a592c
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2025-03-16 14:45:03 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,82 @@
-pins.setPull(DigitalPin.P2, PinPullMode.PullNone)
-let sTime = 0
-let dTime = 5
+function コマンド受信 (コマンド: string) {
+    serial.writeLine(コマンド)
+    cmd = コマンド.split(",")
+    if (cmd[0] == control.deviceName()) {
+        if (cmd[1] == "START") {
+            mode = 1
+            waitTime = parseFloat(cmd[2])
+            dataTime = parseFloat(cmd[3])
+            startTime = input.runningTime()
+            basic.showLeds(`
+                . # # # .
+                # . . . #
+                # . . . #
+                # . . . #
+                . # # # .
+                `)
+        }
+    }
+}
+function 赤外線受信 () {
+    if (input.runningTime() < startTime + waitTime) {
+        if (pins.digitalReadPin(DigitalPin.P0) == 0) {
+            if (dataStratTime == 0) {
+                dataStratTime = input.runningTime()
+            } else {
+                if (input.runningTime() > dataStratTime + dataTime) {
+                    radio.sendString("" + getradiogroup.getHostName() + "," + control.deviceName() + "," + "HIT")
+                    basic.showIcon(IconNames.Chessboard)
+                    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.PowerUp), music.PlaybackMode.UntilDone)
+                    basic.clearScreen()
+                }
+            }
+            led.plot(2, 2)
+        } else {
+            dataStratTime = 0
+            led.unplot(2, 2)
+        }
+    } else {
+        radio.sendString("" + control.deviceName() + "," + getradiogroup.getHostName() + "," + "TO")
+        mode = 0
+        basic.clearScreen()
+    }
+}
+radio.onReceivedString(function (receivedString) {
+    saveString = receivedString
+})
+let startTime = 0
+let waitTime = 0
+let cmd: string[] = []
+let saveString = ""
+let mode = 0
+let dataTime = 0
+let dataStratTime = 0
+serial.redirectToUSB()
+serial.setTxBufferSize(128)
+serial.setRxBufferSize(128)
+pins.setPull(DigitalPin.P0, PinPullMode.PullNone)
+dataStratTime = 0
+dataTime = 5
+mode = 0
+saveString = ""
+let radioGroup = 0
+getradiogroup.initRadioGroup()
 basic.showIcon(IconNames.Heart)
 basic.forever(function () {
-    if (pins.digitalReadPin(DigitalPin.P2) == 0) {
-        if (sTime == 0) {
-            sTime = input.runningTime()
+    if (radioGroup == 0) {
+        radioGroup = getradiogroup.getRadioGroup(saveString)
+        if (radioGroup != 0) {
+            watchfont.showNumber2(radioGroup)
+        }
+        saveString = ""
+    } else {
+        if (saveString != "") {
+            コマンド受信(saveString)
+            saveString = ""
         } else {
-            if (input.runningTime() > sTime + dTime) {
-                basic.showIcon(IconNames.Chessboard)
-                basic.pause(1000)
-                basic.clearScreen()
+            if (mode != 0) {
+                赤外線受信()
             }
         }
-        led.plot(2, 2)
-    } else {
-        sTime = 0
-        led.unplot(2, 2)
     }
 })
